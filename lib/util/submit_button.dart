53354cd15cd8691f9164ea9ef926e0d06095ab92
Philipp <philipp.lerche@sicher3.de>
2022-11-23 13:53:39 +0100

    change submit_button

--- lib/util/submit_button.dart
+++ lib/util/submit_button.dart
@@ -3,38 +3,25 @@ import 'package:flutter/material.dart';
 class SubmitButton extends StatelessWidget {
   final String text;
   final Function()? onPressed;
-  final double borderRadius = 16;
 
   const SubmitButton({super.key, required this.text, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 50,
-      decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-                color: Colors.blue.withOpacity(0.1),
-                blurRadius: 1,
-                offset: const Offset(0, -5))
-          ],
-          borderRadius: BorderRadius.circular(borderRadius),
-          gradient: const LinearGradient(
-              colors: [Colors.orange, Colors.cyan],
-              begin: FractionalOffset(0.1, 0.1),
-              end: FractionalOffset(0.9, 0.9))),
-      child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.transparent,
-              shadowColor: Colors.transparent,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(borderRadius))),
-          onPressed: onPressed,
-          child: Text(
-            text.toUpperCase(),
-            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          )),
+    return SizedBox(
+      width: 170,
+      height: 53,
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        color: Theme.of(context).colorScheme.primary,
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontWeight: FontWeight.w500),
+        ),
+      ),
     );
   }
 }
