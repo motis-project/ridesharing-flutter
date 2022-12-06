548ce4291d19fcb7ceaa1c67d8332700fdf6f6ab
Showcas <cedric.bohni@gmx.de>
2022-12-06 18:06:13 +0100

    save

--- lib/rides/pages/search_card.dart
+++ lib/rides/pages/search_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
 
 class SearchCard extends StatelessWidget {
   const SearchCard({Key? key}) : super(key: key);
--- lib/rides/pages/search_deals_page.dart
+++ lib/rides/pages/search_deals_page.dart
@@ -5,7 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../util/submit_button.dart';
 
 class SearchDealPage extends StatefulWidget {
-  const SearchDealPage(this.start, this.end, this.date, this.seats, {super.key});
+  const SearchDealPage(this.start, this.end, this.date, this.seats,
+      {super.key});
 
   final String start;
   final String end;
@@ -13,7 +14,8 @@ class SearchDealPage extends StatefulWidget {
   final DateTime date;
 
   @override
-  State<SearchDealPage> createState() => _SearchDealPageState(this.start, this.end, this.date, this.seats);
+  State<SearchDealPage> createState() =>
+      _SearchDealPageState(this.start, this.end, this.date, this.seats);
 }
 
 class _SearchDealPageState extends State<SearchDealPage> {
@@ -32,14 +34,15 @@ class _SearchDealPageState extends State<SearchDealPage> {
       body: Column(
         children: [
           //todo Ansicht der Momentanen Suche
-          Container(
-            padding: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.red,  // red as border color
+          const Expanded(
+            child: SizedBox(
+              height: 10.0,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
                 ),
               ),
-              child: Text("Your text...")
+            ),
           ),
           //todo filter der Suchergebnisse
           ElevatedButton(
@@ -48,19 +51,18 @@ class _SearchDealPageState extends State<SearchDealPage> {
                 maximumSize: const Size(500, 20),
               ),
               onPressed: null,
-              child:Text("Filter")
-          ),
+              child: Text("Filter")),
           Expanded(
             child: ListView.separated(
-                padding: const EdgeInsets.all(12),
-                itemCount: 10,
-                separatorBuilder: (context, index) {
-                  return const SizedBox(height: 12);
-                },
-                itemBuilder: (context, index) {
-                  return const SearchCard();
-                },
-              ),
+              padding: const EdgeInsets.all(12),
+              itemCount: 10,
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 12);
+              },
+              itemBuilder: (context, index) {
+                return const SearchCard();
+              },
+            ),
           ),
         ],
       ),
