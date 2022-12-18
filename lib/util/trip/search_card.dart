ec14f975ae5a16011e19188d378f67d5d152e8ca
Showcas <cedric.bohni@gmx.de>
2022-12-18 15:59:20 +0100

    SearchCard cleanup

--- lib/util/trip/search_card.dart
+++ lib/util/trip/search_card.dart
@@ -9,6 +9,106 @@ import '../../rides/pages/search_detail_page.dart';
 class SearchCard extends TripCard<Ride> {
   const SearchCard(super.trip, {super.key});
 
+  FixedTimeline showRoute(context) {
+    return FixedTimeline(
+      theme: CustomTimelineTheme.of(context),
+      children: [
+        TimelineTile(
+          contents: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('${formatTime(trip.startTime)}  ${trip.start}'),
+              ],
+            ),
+          ),
+          node: const TimelineNode(
+            indicator: CustomOutlinedDotIndicator(),
+            endConnector: CustomSolidLineConnector(),
+          ),
+        ),
+        TimelineTile(
+          contents: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('${formatTime(trip.endTime)}  ${trip.end}'),
+              ],
+            ),
+          ),
+          node: const TimelineNode(
+            indicator: CustomOutlinedDotIndicator(),
+            startConnector: CustomSolidLineConnector(),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget showDate() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(formatDate(trip.startTime)),
+        Text("${trip.price}\u{20AC} "),
+      ],
+    );
+  }
+
+  Widget showProfile(driver) {
+    return Row(
+      children: [
+        CircleAvatar(
+          child: Text(driver.username[0]),
+        ),
+        const SizedBox(width: 5),
+        Text(driver.username),
+      ],
+    );
+  }
+
+  Widget showRanking() {
+    return Row(
+      children: const [
+        Text("3"),
+        Icon(
+          Icons.star,
+          color: Colors.amberAccent,
+        ),
+      ],
+    );
+  }
+
+  Widget showCardInfo(context, driver) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: showDate(),
+        ),
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: showRoute(context),
+        ),
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              showProfile(driver),
+              showRanking(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Profile driver = trip.drive!.driver!;
@@ -20,87 +120,7 @@ class SearchCard extends TripCard<Ride> {
             builder: (context) => const SearchDetailPage(),
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(formatDate(trip.startTime)),
-                  Text("${trip.price}\u{20AC} "),
-                ],
-              ),
-            ),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: FixedTimeline(
-                theme: CustomTimelineTheme.of(context),
-                children: [
-                  TimelineTile(
-                    contents: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('${formatTime(trip.startTime)}  ${trip.start}'),
-                        ],
-                      ),
-                    ),
-                    node: const TimelineNode(
-                      indicator: CustomOutlinedDotIndicator(),
-                      endConnector: CustomSolidLineConnector(),
-                    ),
-                  ),
-                  TimelineTile(
-                    contents: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('${formatTime(trip.endTime)}  ${trip.end}'),
-                        ],
-                      ),
-                    ),
-                    node: const TimelineNode(
-                      indicator: CustomOutlinedDotIndicator(),
-                      startConnector: CustomSolidLineConnector(),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        child: Text(driver.username[0]),
-                      ),
-                      const SizedBox(width: 5),
-                      Text(driver.username),
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Text("3"),
-                      Icon(
-                        Icons.star,
-                        color: Colors.amberAccent,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: showCardInfo(context, driver),
       ),
     );
   }
