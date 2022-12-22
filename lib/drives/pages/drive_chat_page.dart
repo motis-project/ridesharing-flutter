6ae42d1ddfbede787fd9b4c84cefdf6acebf9003
Philipp <philipp.lerche@sicher3.de>
2022-12-22 19:31:19 +0100

    cleanup and fix rideOfUserAtTime

--- lib/drives/pages/drive_chat_page.dart
+++ lib/drives/pages/drive_chat_page.dart
@@ -23,11 +23,65 @@ class _DriveChatPageState extends State<DriveChatPage> {
   void initState() {
     super.initState();
     _drive = widget.drive;
-    if (_drive.rides != null) {
-      List<Ride> rides = _drive.rides!;
-      _riders = Set.from(rides.where((ride) => ride.status == RideStatus.approved).map((ride) => ride.rider!));
-      _pendingRides = Set.from(rides.where((ride) => ride.status == RideStatus.pending));
+    _riders = _drive.approvedRides!.map((ride) => ride.rider!).toSet();
+    _pendingRides = _drive.pendingRides!.toSet();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = [];
+    if (_riders.isEmpty && _pendingRides.isEmpty) {
+      widgets.add(const Center(
+        child: Text("No riders or pending rides"),
+      ));
+    } else {
+      if (_riders.isNotEmpty) {
+        List<Widget> riderColumn = [
+          const Text(
+            "Riders",
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          riderList(),
+        ];
+        widgets.addAll(riderColumn);
+      }
+      widgets.add(const SizedBox(height: 10.0));
+      if (_pendingRides.isNotEmpty) {
+        List<Widget> pendingRidesColumn = [
+          const Text(
+            "Pending Rides",
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10.0),
+        ];
+        pendingRidesColumn.addAll(pendingRidesList());
+        widgets.addAll(pendingRidesColumn);
+      }
     }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drive Chat'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: loadDrive,
+        child: ListView.separated(
+          itemCount: widgets.length,
+          itemBuilder: (context, index) {
+            return widgets[index];
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(height: 10);
+          },
+        ),
+      ),
+    );
   }
 
   List<Widget> pendingRidesList() {
@@ -100,62 +154,5 @@ class _DriveChatPageState extends State<DriveChatPage> {
       _riders = _drive.approvedRides!.map((ride) => ride.rider!).toSet();
       _pendingRides = _drive.pendingRides!.toSet();
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> widgets = [];
-    if (_riders.isEmpty && _pendingRides.isEmpty) {
-      widgets.add(const Center(
-        child: Text("No riders or pending rides"),
-      ));
-    } else {
-      if (_riders.isNotEmpty) {
-        List<Widget> riderColumn = [
-          const Text(
-            "Riders",
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          riderList(),
-        ];
-        widgets.addAll(riderColumn);
-      }
-      widgets.add(const SizedBox(height: 10.0));
-      if (_pendingRides.isNotEmpty) {
-        List<Widget> pendingRidesColumn = [
-          const Text(
-            "Pending Rides",
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10.0),
-        ];
-        pendingRidesColumn.addAll(pendingRidesList());
-        widgets.addAll(pendingRidesColumn);
-      }
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Drive Chat'),
-      ),
-      body: RefreshIndicator(
-        onRefresh: loadDrive,
-        child: ListView.separated(
-          itemCount: widgets.length,
-          itemBuilder: (context, index) {
-            return widgets[index];
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(height: 10);
-          },
-        ),
-      ),
-    );
   }
 }
