331fd1f771b09a460d70416adb020b0567eb717c
Simon Hock <simon.hock@stud.tu-darmstadt.de>
2023-01-10 19:43:19 +0100

    Add about page

--- lib/account/pages/about_page.dart
+++ lib/account/pages/about_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -9,6 +12,18 @@ class AboutPage extends StatefulWidget {
 }
 
 class _AboutPageState extends State<AboutPage> {
+  PackageInfo? _packageInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    PackageInfo.fromPlatform().then(
+      (value) => setState(
+        () => _packageInfo = value,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +31,79 @@ class _AboutPageState extends State<AboutPage> {
         title: Text(S.of(context).pageAboutTitle),
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Text(S.of(context).appName),
+        child: CustomScrollView(
+          physics: const ClampingScrollPhysics(),
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(children: [
+                  Expanded(child: Container()),
+                  Text(
+                    S.of(context).appName,
+                    style: Theme.of(context).primaryTextTheme.displaySmall,
+                  ),
+                  if (_packageInfo != null) ...[
+                    Text(
+                      S.of(context).pageAboutVersion(_packageInfo!.version),
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .subtitle2!
+                          .copyWith(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5)),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      S.of(context).pageAboutBuildNumber(_packageInfo!.buildNumber),
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .subtitle2!
+                          .copyWith(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5)),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      S.of(context).pageAboutBuildSignature(_packageInfo!.buildSignature),
+                      style: Theme.of(context)
+                          .primaryTextTheme
+                          .subtitle2!
+                          .copyWith(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5)),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                  SvgPicture.asset('assets/logo.svg',
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: MediaQuery.of(context).size.width / 2,
+                      semanticsLabel: 'MOTIS Logo'),
+                  Text(
+                    S.of(context).pageAboutText,
+                    textAlign: TextAlign.center,
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                        TextButton(
+                          onPressed: () async => await launchUrlString(
+                              'https://github.com/MOTIS-Mitfahr-App/flutter-app',
+                              mode: LaunchMode.externalApplication),
+                          child: Text(S.of(context).pageAboutGithub),
+                        ),
+                        TextButton(
+                          onPressed: () async => await launchUrlString('https://motis-project.de/'),
+                          child: Text(S.of(context).pageAboutWebsite),
+                        ),
+                        TextButton(
+                          // TODO Terms of service
+                          onPressed: () async => await launchUrlString('https://motis-project.de/'),
+                          child: Text(S.of(context).pageAboutTermsOfService),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+          ],
         ),
       ),
     );
