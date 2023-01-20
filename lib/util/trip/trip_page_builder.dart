2892b9965ad47bee54e3211033fa3eae96cc2168
Fynn Starke <f.starke@iot-venture.com>
2023-01-20 19:12:09 +0100

    Fix capitalization of "No rides" text

--- lib/util/trip/trip_page_builder.dart
+++ lib/util/trip/trip_page_builder.dart
@@ -4,26 +4,23 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../rides/models/ride.dart';
 import 'trip.dart';
-import 'trip_card.dart';
 import 'trip_stream_builder.dart';
 
-class TripPageBuilder {
-  const TripPageBuilder();
+class TripPageBuilder<T extends Trip> extends StatelessWidget {
+  final Stream<List<T>> trips;
+  final Function() onFabPressed;
 
-  static Widget build<T extends Trip>(
-    BuildContext context,
-    String title,
-    Stream<List<T>> trips,
-    TripCard<T> Function(T) tripCard,
-    FloatingActionButton floatingActionButton,
-  ) {
-    final String name = title.toLowerCase();
+  const TripPageBuilder(this.trips, {super.key, required this.onFabPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isRide = T == Ride;
 
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(isRide ? S.of(context).pageRidesTitle : S.of(context).pageDrivesTitle),
           bottom: TabBar(
             labelColor: Theme.of(context).colorScheme.onSurface,
             tabs: <Tab>[
@@ -38,35 +35,47 @@ class TripPageBuilder {
             children: <Widget>[
               TripStreamBuilder<T>(
                 stream: trips,
-                emptyMessage: S.of(context).widgetTripBuilderNoUpcoming(name),
-                filterTrips: (List<T> trips) => trips
-                    .where(
-                      (T trip) =>
-                          trip.endTime.isAfter(DateTime.now()) &&
-                          (trip is! Ride || trip.status != RideStatus.withdrawnByRider) &&
-                          !trip.hideInListView,
-                    )
-                    .toList(),
-                tripCard: tripCard,
+                emptyMessage: isRide
+                    ? S.of(context).widgetTripBuilderNoUpcomingRides
+                    : S.of(context).widgetTripBuilderNoUpcomingDrives,
+                filterTrips: (List<T> trips) =>
+                    trips.where((T trip) => trip.shouldShowInListView(past: false)).toList(),
               ),
               TripStreamBuilder<T>(
                 stream: trips,
-                emptyMessage: S.of(context).widgetTripBuilderNoPast(name),
-                filterTrips: (List<T> trips) => trips.reversed
-                    .where(
-                      (T trip) =>
-                          trip.endTime.isBefore(DateTime.now()) &&
-                          (trip is! Ride || trip.status != RideStatus.withdrawnByRider) &&
-                          !trip.hideInListView,
-                    )
-                    .toList(),
-                tripCard: tripCard,
+                emptyMessage:
+                    isRide ? S.of(context).widgetTripBuilderNoPastRides : S.of(context).widgetTripBuilderNoPastDrives,
+                filterTrips: (List<T> trips) =>
+                    trips.reversed.where((T trip) => trip.shouldShowInListView(past: true)).toList(),
               ),
             ],
           ),
         ),
-        floatingActionButton: Semantics(sortKey: const OrdinalSortKey(0), child: floatingActionButton),
+        floatingActionButton: Semantics(
+          sortKey: const OrdinalSortKey(0),
+          child: getFloatingActionButton(context, isRide: isRide),
+        ),
       ),
     );
+  }
+
+  Widget getFloatingActionButton(BuildContext context, {required bool isRide}) {
+    if (isRide) {
+      return FloatingActionButton(
+        heroTag: 'RideFAB',
+        tooltip: S.of(context).pageRidesTooltipSearchRide,
+        onPressed: onFabPressed,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(Icons.search),
+      );
+    } else {
+      return FloatingActionButton(
+        heroTag: 'DriveFAB',
+        tooltip: S.of(context).pageDrivesTooltipOfferRide,
+        onPressed: onFabPressed,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(Icons.add),
+      );
+    }
   }
 }
