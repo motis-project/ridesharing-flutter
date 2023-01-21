7fa8f00b90821802264cd6070e78f888fd9ab419
Philipp <philipp.lerche@sicher3.de>
2023-01-21 20:54:36 +0100

    fix ride query

--- lib/rides/pages/ride_detail_page.dart
+++ lib/rides/pages/ride_detail_page.dart
@@ -50,6 +50,10 @@ class _RideDetailPageState extends State<RideDetailPage> {
         *,
         drive: drive_id(
           $_driveQuery
+        ),
+        chat: chats!chats_ride_id_fkey(
+          *,
+          messages: messages!messages_chat_id_fkey(*)
         )
       ''';
 
