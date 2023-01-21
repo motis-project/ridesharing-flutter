1020e36661ce52e6a857ad8f32ae9c02f9890189
Simon Hock <simon.hock@stud.tu-darmstadt.de>
2023-01-21 16:02:07 +0100

    Fix avatar heros

--- lib/util/profiles/profile_widget.dart
+++ lib/util/profiles/profile_widget.dart
@@ -13,6 +13,7 @@ class ProfileWidget extends StatelessWidget {
   final bool isTappable;
   final Function()? onTap;
   final Function(dynamic)? onPop;
+  final bool withHero;
 
   const ProfileWidget(
     this.profile, {
@@ -23,6 +24,7 @@ class ProfileWidget extends StatelessWidget {
     this.isTappable = true,
     this.onTap,
     this.onPop,
+    this.withHero = false,
   })  : assert(onTap == null || onPop == null, 'onTap and onPop cannot be set at the same time.'),
         assert((onTap == null && onPop == null) || isTappable, 'isTappable has to be true if onTap or onPop are set');
 
@@ -35,7 +37,7 @@ class ProfileWidget extends StatelessWidget {
       tooltip: S.of(context).seeProfile,
       child: Row(
         children: <Widget>[
-          Avatar(profile, size: size),
+          Avatar(profile, size: size, withHero: withHero),
           const SizedBox(width: 5),
           Text(profile.username, style: TextStyle(fontSize: size)),
         ],
