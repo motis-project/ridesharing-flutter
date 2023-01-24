29a4d01cb3cb3d0ef290d681dc2ed40b339a06b9
Eddie <69083092+Eddie-42@users.noreply.github.com>
2023-01-24 12:06:54 +0100

    Add support for coverage for windows (#79)
    Co-authored-by: Simon Hock <simon.hock@stud.tu-darmstadt.de>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,8 @@
 ./scripts/coverage_helper.sh
 flutter test --coverage
 genhtml coverage/lcov.info -o coverage/html
-open coverage/html/index.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    open coverage/html/index.html
+else
+    start coverage/html/index.html
+fi
