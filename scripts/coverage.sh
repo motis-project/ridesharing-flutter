d074ebcc9591e3f3bff7513a9c74c0f49f909948
Fynn Starke <f.starke@iot-venture.com>
2023-02-03 23:30:55 +0100

    Fix script for fvm

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,6 +1,12 @@
 ./scripts/coverage_helper.sh
-flutter test --coverage
+if command -v fvm &> /dev/null; then
+    fvm flutter test --coverage
+else
+    flutter test --coverage
+fi
+    
 genhtml coverage/lcov.info -o coverage/html
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     open coverage/html/index.html
 else
