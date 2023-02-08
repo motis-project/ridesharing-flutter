bc44c5ebe4053f1551c0a119d0b5e1ac74f94188
Simon Hock <simon.hock@stud.tu-darmstadt.de>
2023-02-08 21:04:31 +0100

    Increase padding

--- lib/drives/pages/create_drive_page.dart
+++ lib/drives/pages/create_drive_page.dart
@@ -240,7 +240,7 @@ class _CreateDriveFormState extends State<CreateDriveForm> {
               ),
             ],
           ),
-          const SizedBox(height: 5),
+          const SizedBox(height: 10),
           SizedBox(
             width: 150,
             child: IncrementField(
