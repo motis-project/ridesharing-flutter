ea215105433a6d078177e8a7142d6f5e46b4d5c3
Philipp <philipp.lerche@sicher3.de>
2023-02-12 17:22:05 +0100

    add firebase

---
+++ lib/util/firebase.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+
+import 'supabase_manager.dart';
+
+FirebaseMessaging messaging = FirebaseMessaging.instance;
+FirebaseManager firebaseManager = FirebaseManager();
+
+class FirebaseManager {
+  FirebaseMessaging? messaging;
+  NotificationSettings? settings;
+
+  Future<void> initialize() async {
+    if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+      await Firebase.initializeApp();
+      messaging = FirebaseMessaging.instance;
+      // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      //   print('A new onMessageOpenedApp event was published!');
+      // });
+      FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+    }
+  }
+}
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+Future<void> requestPermissionAndloadPushToken() async {
+  // at the moment Push Notifications are only supported on Android
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    firebaseManager.settings = await firebaseManager.messaging!.requestPermission();
+
+    if (firebaseManager.settings!.authorizationStatus == AuthorizationStatus.authorized) {
+      final String? fcmToken = await firebaseManager.messaging!.getToken();
+      await supabaseManager.supabaseClient.from('push_tokens').upsert(
+        <String, dynamic>{
+          'token': fcmToken,
+          'user_id': supabaseManager.currentProfile?.id,
+        },
+        onConflict: 'token',
+      );
+    }
+  }
+}
+
+Future<void> deletePushToken() async {
+  // at the moment Push Notifications are only supported on Android
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    final String? fcmToken = await firebaseManager.messaging!.getToken();
+    await supabaseManager.supabaseClient.from('push_tokens').delete().eq('token', fcmToken);
+  }
+}
