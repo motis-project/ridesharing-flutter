a27dd37ac7bd0a02c5ba0e5d8063918126846abb
Simon Hock <simon.hock@stud.tu-darmstadt.de>
2023-02-22 18:39:15 +0100

    Recurring Drives List View

--- lib/util/trip/trip_stream_builder.dart
+++ lib/util/trip/trip_stream_builder.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 
-import '../../drives/models/drive.dart';
-import '../../rides/models/ride.dart';
-import 'drive_card.dart';
-import 'ride_card.dart';
-import 'trip.dart';
-
-class TripStreamBuilder<T extends Trip> extends StreamBuilder<List<T>> {
+class TripStreamBuilder<T> extends StreamBuilder<List<T>> {
   TripStreamBuilder({
     super.key,
     super.stream,
     required String emptyMessage,
-    required List<T> Function(List<T> trips) filterTrips,
+    List<T> Function(List<T> trips)? filterTrips,
+    required Widget Function(T trip) itemBuilder,
   }) : super(
           builder: (BuildContext context, AsyncSnapshot<List<T>> snapshot) {
             if (snapshot.hasData) {
               final List<T> trips = snapshot.data!;
-              final List<T> filteredTrips = filterTrips(trips);
+              final List<T> filteredTrips = filterTrips != null ? filterTrips(trips) : trips;
               return filteredTrips.isEmpty
                   ? Center(key: const Key('emptyMessage'), child: Text(emptyMessage))
                   : ListView.separated(
@@ -24,7 +19,7 @@ class TripStreamBuilder<T extends Trip> extends StreamBuilder<List<T>> {
                       itemBuilder: (BuildContext context, int index) {
                         final T trip = filteredTrips[index];
 
-                        return trip is Ride ? RideCard(trip) : DriveCard(trip as Drive);
+                        return itemBuilder(trip);
                       },
                       separatorBuilder: (BuildContext context, int index) {
                         return const SizedBox(height: 10);
