50736ee8353e40b222880e14c67bd0cdcc25e120
Simon Hock <simon.hock@stud.tu-darmstadt.de>
2023-03-13 12:29:11 +0100

    Refactor helper method

--- test/util/expect_meets_accessibility_guidelines.dart
+++ test/util/expect_meets_accessibility_guidelines.dart
@@ -1,9 +1,1 @@
 import 'package:flutter_test/flutter_test.dart';
-
-Future<void> checkMeetsAccessibility(WidgetTester tester) async {
-  final SemanticsHandle handle = tester.ensureSemantics();
-  await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
-  await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
-  await expectLater(tester, meetsGuideline(textContrastGuideline));
-  handle.dispose();
-}
--- test/util/pump_material.dart
+++ test/util/pump_material.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:motis_mitfahr_app/util/locale_manager.dart';
+import 'package:motis_mitfahr_app/util/theme_manager.dart';
 
 Future<void> pumpMaterial(
   WidgetTester tester,
   Widget widget, {
   NavigatorObserver? navigatorObserver,
+  ThemeData? themeData,
 }) async {
   await tester.pumpWidget(
     MaterialApp(
@@ -15,6 +17,7 @@ Future<void> pumpMaterial(
       locale: localeManager.currentLocale,
       navigatorObservers: navigatorObserver == null ? [] : [navigatorObserver],
       home: widget,
+      theme: themeData,
     ),
   );
 }
@@ -25,4 +28,16 @@ Future<void> pumpScaffold(WidgetTester tester, Widget widget) async {
 
 Future<void> pumpForm(WidgetTester tester, Widget widget, {required Key formKey}) async {
   await pumpScaffold(tester, Form(key: formKey, child: widget));
+}
+
+Future<void> expectMeetsAccessibilityGuidelines(WidgetTester tester, Widget widget) async {
+  final SemanticsHandle handle = tester.ensureSemantics();
+  for (final ThemeData theme in [themeManager.lightTheme, themeManager.darkTheme]) {
+    await pumpMaterial(tester, widget, themeData: theme);
+    await tester.pump();
+    await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+    await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+    await expectLater(tester, meetsGuideline(textContrastGuideline));
+  }
+  handle.dispose();
 }
